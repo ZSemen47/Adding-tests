a2665da248c13321c949883951ae88cacb473f5c
Semen Zazulin <zazulinsemen@gmail.com>
2020-09-28 16:10:51 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registred = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registred);
+        boolean passed = expected == boolean;
+        System.out.println(passed);
+    }
+}
