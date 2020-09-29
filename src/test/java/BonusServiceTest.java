559f88228ec666059af4057c0372da11214f5476
Semen Zazulin <zazulinsemen@gmail.com>
2020-09-29 23:22:01 +0300

    test2

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -2,8 +2,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
-    @org.junit.jupiter.api.Test //это аннотация
-    void calculate() { // void означает что метод ничего не возвращает
+    @org.junit.jupiter.api.Test
+    void calculate() {
         BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registred = true;
