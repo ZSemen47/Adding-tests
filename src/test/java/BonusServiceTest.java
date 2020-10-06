8bc389092fd26256032b15080e6e3c6209fe27e6
Semen Zazulin <zazulinsemen@gmail.com>
2020-10-06 19:05:35 +0300

    Release

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,11 +3,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BonusServiceTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateForRegistredAndUnderLimit() {
         BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registred = true;
         long expected = 30;
+        long actual = service.calculate(amount, registred);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegistredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000_60;
+        boolean registred = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registred);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForNonregistredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registred = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registred);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForNonRegistredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000_60;
+        boolean registred = false;
+        long expected = 500;
         long actual = service.calculate(amount, registred);
         assertEquals(expected, actual);
     }
